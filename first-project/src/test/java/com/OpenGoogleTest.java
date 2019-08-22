2f22ab1704df0ca91988ddeca816030b6077448e
tauha11 <54038344+tauha11@users.noreply.github.com>
2019-08-22 20:09:35 +0300

    Added clickImFillingLuck

--- first-project/src/test/java/com/OpenGoogleTest.java
+++ first-project/src/test/java/com/OpenGoogleTest.java
@@ -1,21 +1,44 @@
 package com;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 public class OpenGoogleTest {
     WebDriver driver;
-    @Test
 
+    @BeforeMethod
     public void setUp()
     {
         driver = new ChromeDriver();
-        driver.get("www.google.com");
-        //driver.navigate().to;
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("https://www.google.com");
     }
-    driver.findElement(By.name("q")).sendKeys("java");
-    Thread.sleep(7000);
-    driver.quit();
+
+    @Test
+    public void searchJavaTest() throws InterruptedException {
+        driver.findElement(By.name("q")).click();
+        driver.findElement(By.name("q")).clear();
+        driver.findElement(By.name("q")).sendKeys("java" + Keys.ENTER);
+        Thread.sleep(7000);
+    }
+    @Test
+    public void clickImFillingLuck()
+    {
+        new Actions(driver).moveToElement(driver.findElement(By.name("btnI"))).pause(2).click();
+        driver.findElement(By.name("btnI")).click();
+
+    }
+    @AfterMethod
+    public void tearDown()
+    {
+        driver.quit();
+    }
 }
